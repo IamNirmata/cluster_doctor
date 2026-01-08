68b890e63e88c5eadbf5a3137b59a620d5dc241b
IamNirmata <work.harified@gmail.com>
2026-01-08 15:49:53 -0800

    Auto commit at 20260108_154953

--- kubectl/functions.py
+++ kubectl/functions.py
@@ -68,7 +68,8 @@ def get_db_latest_status(pod=DEFAULT_POD, namespace=DEFAULT_NAMESPACE, db_path=D
     """)
     return _exec_python_on_pod(code, pod, namespace)
 
-#3 Build prio
+#3 Build priority list
+Build Priority Queue
 
 
 
