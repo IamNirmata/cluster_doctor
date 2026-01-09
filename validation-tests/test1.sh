899b305922096b7833b3f9366870fc3cb2b91728
IamNirmata <work.harified@gmail.com>
2026-01-08 18:09:23 -0800

    Auto commit at 20260108_180923

--- validation-tests/test1.sh
+++ validation-tests/test1.sh
@@ -1,7 +1,7 @@
 echo "test1.sh executed"
 export gcr_test_result="pass"
 
-python /optcluster_doctor/utils/functions.py add-result \
+python /opt/cluster_doctor/utils/functions.py add-result \
     "$GCRNODE" \
     "test1" \
     "pass" \
