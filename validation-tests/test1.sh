922b175e1e8d977bc4476bafe7f5760bf9d00a35
IamNirmata <work.harified@gmail.com>
2026-01-08 18:12:00 -0800

    Auto commit at 20260108_181200

--- validation-tests/test1.sh
+++ validation-tests/test1.sh
@@ -1,9 +1,4 @@
 echo "test1.sh executed"
 export gcr_test_result="pass"
 
-python /opt/cluster_doctor/utils/functions.py add-result \
-    "$GCRNODE" \
-    "test1" \
-    "pass" \
-    "$GCRTIME" \
-    --db-path /data/continuous_validation/metadata/validation.db
+
