daa90d9608076e35a5136cf836a9d72c2887032e
IamNirmata <work.harified@gmail.com>
2026-01-08 18:23:33 -0800

    Auto commit at 20260108_182333

--- validation-tests/test1.sh
+++ validation-tests/test1.sh
@@ -1,5 +1,5 @@
 mkdir -p /data/continuous_validation/test1/$GCRNODE
-echo "test1.sh executed" | tee /data/continuous_validation/
+echo "test1.sh executed" | tee /data/continuous_validation/test1/$GCRNODE/output.log
 export gcr_test_result="pass"
 
 
