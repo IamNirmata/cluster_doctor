ad40ca84234f527044e9485401b3e61b6bbb4d1a
IamNirmata <work.harified@gmail.com>
2026-01-08 18:04:55 -0800

    Auto commit at 20260108_180455

--- validation-tests/test1.sh
+++ validation-tests/test1.sh
@@ -3,6 +3,6 @@ export gcr_test_result="pass"
 
 python cluster_doctor/utils/functions.py --add_result_local \
   --db_path /data/continuous_validation/metadata/validation.db \
-  --node_name "$(hostname)" \
+  --node_name "$GCRNODE" \
   --test_name "test1" \
   --result "${gcr_test_result}"
