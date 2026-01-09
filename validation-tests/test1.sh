80a6676d8c1479ac896fc5892b604c3ae8519745
IamNirmata <work.harified@gmail.com>
2026-01-08 20:02:46 -0800

    Auto commit at 20260108_200246

--- validation-tests/test1.sh
+++ validation-tests/test1.sh
@@ -1,7 +1,7 @@
 mkdir -p /data/continuous_validation/storage/$GCRNODE
 apt-get update && apt-get install -y fio
 
-bash /workspace/cluster_doctor/validation-tests/storage.sh | tee /data/continuous_validation/storage/$GCRNODE/test1-$GCRNODE-$GCRTIME.log
+bash /workspace/cluster_doctor/validation-tests/storage.sh | tee /data/continuous_validation/storage/$GCRNODE/s-$GCRNODE-$GCRTIME.log
 export gcr_test_result="pass"
 
 
