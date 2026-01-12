3aa6660f065e1581aa34f34beaa2bbb364a0d873
IamNirmata <work.harified@gmail.com>
2026-01-12 15:24:25 -0800

    Auto commit at 20260112_152425

--- validation-tests/db-update.sh
+++ validation-tests/db-update.sh
@@ -2,6 +2,6 @@
 python /workspace/c-val/utils/functions.py add-result \
     "$GCRNODE" \
     "storage" \
-    "ran" \
+    "pass" \
     "$GCRTIME" \
     --db-path /data/continuous_validation/metadata/validation.db
