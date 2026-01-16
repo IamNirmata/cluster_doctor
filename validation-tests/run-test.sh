b7bbee7ce21a679857de575ff4b1c554dc120b72
IamNirmata <work.harified@gmail.com>
2026-01-16 11:28:14 -0800

    Auto commit at 20260116_112814

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -3,6 +3,7 @@ mkdir -p /data/continuous_validation/storage/$GCRNODE/storage-$GCRNODE-$GCRTIME/
 mkdir -p /data/continuous_validation/nccl/$GCRNODE/nccl-$GCRNODE-$GCRTIME/
 apt-get update && apt-get install -y fio
 
+
 bash /workspace/c-val/validation-tests/storage/storage.sh | tee /data/continuous_validation/storage/$GCRNODE/storage-$GCRNODE-$GCRTIME/storage-$GCRNODE-$GCRTIME.log
 $GCRRESULT1=pass
 
