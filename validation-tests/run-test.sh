725bc343a1f15396a1a56ece7dcc6835055bba2e
IamNirmata <work.harified@gmail.com>
2026-01-16 10:53:12 -0800

    Auto commit at 20260116_105312

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -6,6 +6,6 @@ bash /workspace/c-val/validation-tests/storage/storage.sh | tee /data/continuous
 $GCRRESULT1=pass
 
 NCCL_NET=IB  NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1 NCCL_DEBUG=INFO torchrun --nproc_per_node=8 single-node-allreduce.py 2>&1 | tee /data/continuous_validation/nccl/$GCRNODE/nccl-$GCRNODE-$GCRTIME/nccl-$GCRNODE-$GCRTIME.log
-
+$GCRRESULT2=pass
 
 
