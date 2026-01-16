5fd45123b4681d4cdca0292cc87d5dbfb7f93404
IamNirmata <work.harified@gmail.com>
2026-01-16 10:51:14 -0800

    Auto commit at 20260116_105114

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -4,6 +4,6 @@ apt-get update && apt-get install -y fio
 bash /workspace/c-val/validation-tests/storage/storage.sh | tee /data/continuous_validation/storage/$GCRNODE/storage-$GCRNODE-$GCRTIME/storage-$GCRNODE-$GCRTIME.log
 $GCRRESULT1=pass
 
-
+NCCL_NET=IB  NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1 NCCL_DEBUG=INFO torchrun --nproc_per_node=8 single-node-allreduce.py 
 
 
