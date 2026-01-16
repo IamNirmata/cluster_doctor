71736d30a1caad392584ca5902b129501df1d0e9
IamNirmata <work.harified@gmail.com>
2026-01-16 12:16:25 -0800

    Auto commit at 20260116_121625

--- validation-tests/db-update.sh
+++ validation-tests/db-update.sh
@@ -2,8 +2,8 @@
 echo "Updating main db with all test results"
 STORAGE_OUTPUT_DIR="/data/continuous_validation/storage/$GCRNODE/storage-$GCRNODE-$GCRTIME"
 echo "Storage Output dir: $STORAGE_OUTPUT_DIR"
-NCCl_OUTPUT_DIR="/data/continuous_validation/nccl/$GCRNODE/nccl-$GCRNODE-$GCRTIME"
-echo "NCCL Output dir: $NCCl_OUTPUT_DIR"
+NCCL_OUTPUT_DIR="/data/continuous_validation/nccl/$GCRNODE/nccl-$GCRNODE-$GCRTIME"
+echo "NCCL Output dir: $NCCL_OUTPUT_DIR"
 
 #main DB update
 python /workspace/c-val/utils/functions.py add-result \
