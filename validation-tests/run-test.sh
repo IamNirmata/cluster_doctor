0da6f4d4e0bd643e85e4cd27d96ced9679ba4489
IamNirmata <work.harified@gmail.com>
2026-01-19 15:12:15 -0800

    Auto commit at 20260119_151215

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -7,6 +7,7 @@ echo "STORAGE_OUTPUT_DIR: $STORAGE_OUTPUT_DIR"
 echo "NCCL_OUTPUT_DIR: $NCCL_OUTPUT_DIR"
 
 echo "NCCL_LOG_FILE: $NCCL_LOG_FILE"
+echo "NCCL_SUMMARY_FILE: $NCCL_SUMMARY_FILE"
 
 echo "#########################################################################"
 echo "Running tests on node: $GCRNODE at time: $GCRTIME"
