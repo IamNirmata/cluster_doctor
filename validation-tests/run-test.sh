eb957d380d0b23559dc8c396dfe64350ce1665ac
IamNirmata <work.harified@gmail.com>
2026-01-19 15:11:32 -0800

    Auto commit at 20260119_151132

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -3,7 +3,7 @@
 #prep
 apt-get update > /dev/null 2>&1 && apt-get install -y fio > /dev/null 2>&1 
 
-ec
+echo "
 
 echo "#########################################################################"
 echo "Running tests on node: $GCRNODE at time: $GCRTIME"
