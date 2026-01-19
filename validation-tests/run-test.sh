b543360edec3bae47cbb8b450db6e0e16863c28a
IamNirmata <work.harified@gmail.com>
2026-01-19 15:01:18 -0800

    Auto commit at 20260119_150118

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -23,7 +23,7 @@ export GCRRESULT1=pass
 
 #nccl test
 nccl-log-file= "$nccl_dir/nccl-$GCRNODE-$GCRTIME.log"
-nccl
+nccl-test
 NCCL_NET=IB  NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1 NCCL_DEBUG=INFO torchrun --nproc_per_node=8 /workspace/c-val/validation-tests/nccl/single-node-allreduce.py 2>&1 | tee "$nccl-log-file"
 echo "NCCL test is complete. Log file: $nccl-log-file"
 export GCRRESULT2=pass
