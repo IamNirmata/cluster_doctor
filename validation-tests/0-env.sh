b374adc47658ba518e8152de1a158639ca370a18
IamNirmata <work.harified@gmail.com>
2026-01-19 15:10:55 -0800

    Auto commit at 20260119_151055

--- validation-tests/0-env.sh
+++ validation-tests/0-env.sh
@@ -12,4 +12,4 @@ export NCCL_LOG_FILE="$NCCL_OUTPUT_DIR/nccl-$GCRNODE-$GCRTIME.log"
 
 #summary files
 export NCCL_SUMMARY_FILE="$NCCL_OUTPUT_DIR/nccl-summary-$GCRNODE-$GCRTIME.json"
-ex
+export STORAGE_SUMMARY_FILE="$STORAGE_OUTPUT_DIR/storage-summary-$GCRNODE-$GCRTIME.json"
