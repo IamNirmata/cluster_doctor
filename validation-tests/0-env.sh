2ac26d649bbe501ce14b113ab42728bb59c2912c
IamNirmata <work.harified@gmail.com>
2026-01-19 15:05:55 -0800

    Auto commit at 20260119_150555

--- validation-tests/0-env.sh
+++ validation-tests/0-env.sh
@@ -2,3 +2,4 @@
 export NCCL_OUTPUT_DIR="/workspace/c-val/validation-tests/logs"
 export STORAGE_OUTPUT_DIR="/workspace/c-val/validation-tests/logs"/storage
 mkdir -p $NCCL_OUTPUT_DIR
+mkdir -p $STORAGE_OUTPUT_DIR
