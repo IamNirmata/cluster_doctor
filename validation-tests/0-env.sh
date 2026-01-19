a181b63eca03857232d109adf4f3174129f72361
IamNirmata <work.harified@gmail.com>
2026-01-19 15:06:56 -0800

    Auto commit at 20260119_150656

--- validation-tests/0-env.sh
+++ validation-tests/0-env.sh
@@ -1,4 +1,3 @@
 #setup directories and environment variables
 export STORAGE_OUTPUT_DIR="/data/continuous_validation/storage/$GCRNODE/storage-$GCRNODE-$GCRTIME"
-
 export NCCL_OUTPUT_DIR="/data/continuous_validation/nccl/$GCRNODE/nccl-$GCRNODE-$GCRTIME"
