aee5a0707a66b44dd5dc42a869ebe0a3537cf884
IamNirmata <work.harified@gmail.com>
2026-01-19 15:11:48 -0800

    Auto commit at 20260119_151148

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -3,7 +3,7 @@
 #prep
 apt-get update > /dev/null 2>&1 && apt-get install -y fio > /dev/null 2>&1 
 
-echo "
+echo "STORAGE_OUTPUT_DIR
 
 echo "#########################################################################"
 echo "Running tests on node: $GCRNODE at time: $GCRTIME"
