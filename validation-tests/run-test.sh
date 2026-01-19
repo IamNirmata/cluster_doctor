460cff5cae7ce89cb308eefb6e40e5505efe6a83
IamNirmata <work.harified@gmail.com>
2026-01-19 14:50:36 -0800

    Auto commit at 20260119_145036

--- validation-tests/run-test.sh
+++ validation-tests/run-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #prep
-apt-get update && apt-get install -y fio
+apt-get update && apt-get install -y fio 
 
 #create dirs
 storage_dir="/data/continuous_validation/storage/$GCRNODE/storage-$GCRNODE-$GCRTIME/"
