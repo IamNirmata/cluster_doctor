d6e5f582be733f0cad3101680bf209a132ba3cf6
IamNirmata <work.harified@gmail.com>
2026-01-19 19:32:15 -0800

    Auto commit at 20260119_193215

--- validation-tests/dltest/dltest.sh
+++ validation-tests/dltest/dltest.sh
@@ -4,7 +4,7 @@ echo "Running DL Test on node: $GCRNODE at time: $GCRTIME"
 DLTEST_COMMAND="/data/continuous_validation/deeplearning_unit_test/main.py"
 
 
-cd 
+cd /data/continuous_validation/deeplearning_unit_test || exit 1
 torchrun --nnodes=1 --nproc-per-node "$1" "$DLTEST_COMMAND" \
   --test_plan 80gb-b200 \
   --baseline_test_id b200-pt2.8.0-cuda12.9 \
