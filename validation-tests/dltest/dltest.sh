dc2cd175238ffa01d51634050cc50d7bfc825ddc
IamNirmata <work.harified@gmail.com>
2026-01-19 16:01:49 -0800

    Auto commit at 20260119_160149

--- validation-tests/dltest/dltest.sh
+++ validation-tests/dltest/dltest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running DL Test on node: $GCRNODE at time: $GCRTIME"
-DLTEST_SCRIPT="/data/continuous_validation/deeplearning_unit_test"
+DLTEST_C="/data/continuous_validation/deeplearning_unit_test"
 
 
 
