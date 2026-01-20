ae49e79c49921f94b4ea4329bf9d9c29d1ac2ff7
IamNirmata <work.harified@gmail.com>
2026-01-19 16:02:06 -0800

    Auto commit at 20260119_160206

--- validation-tests/dltest/dltest.sh
+++ validation-tests/dltest/dltest.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running DL Test on node: $GCRNODE at time: $GCRTIME"
-DLTEST_COMMAND="torchrun --nnodes=1 --nproc-per-node "$1" main.py \
-  --test_plan 80gb-b200 \
-  --baseline_test_id b200-pt2.8.0-cuda12.9 \
-  --iterations 20 \
-  >"$log" 2>&1
-
-rc=$?
+DLTEST_COMMAND="
 
 
 
