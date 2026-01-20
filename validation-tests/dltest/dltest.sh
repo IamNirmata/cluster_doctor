a952a96f6257fe2173c34c172339d32a024cb0ca
IamNirmata <work.harified@gmail.com>
2026-01-19 16:02:32 -0800

    Auto commit at 20260119_160232

--- validation-tests/dltest/dltest.sh
+++ validation-tests/dltest/dltest.sh
@@ -5,8 +5,7 @@ DLTEST_COMMAND="/data/continuous_validation/deeplearning_unit_test/main.py"
 
 
 
-
-torchrun --nnodes=1 --nproc-per-node "$1" main.py \
+torchrun --nnodes=1 --nproc-per-node "$1" "$DLTEST_COMMAND" \
   --test_plan 80gb-b200 \
   --baseline_test_id b200-pt2.8.0-cuda12.9 \
   --iterations 20 \
